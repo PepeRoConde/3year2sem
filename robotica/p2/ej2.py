17b9f56aaf29291b7ca63616545f4d2fdc96fe67
pabloChantada <pablo.chantada@udc.es>
2025-03-10 20:40:13 +0100

    robotica: ej2 comienzo

---
+++ robotica/p2/ej2.py
@@ -0,0 +1,75 @@
+from robobopy.Robobo import Robobo
+from robobosim.RoboboSim import RoboboSim
+from robobopy.utils.IR import IR
+from robobopy.utils.BlobColor import BlobColor
+import time
+
+# USAR UN PI -> PRECISION SIN OSCILACIONES GRADNES
+
+
+SPEED = 5 
+TIME = 2
+VERY_SHORT = 25 
+IP = 'localhost'
+KP = 1
+task_completed = False
+
+def moveToAColor(goal):
+    '''
+    Mueve el robot hasta llegar a una distancia indicada
+    '''
+    # APLICAR EL PI AQUI
+    print("Distance: ", robobo.readIRSensor(IR.FrontC))
+    error = goal - robobo.readIRSensor(IR.FrontC)
+    speed = error * KP
+    robobo.moveWheelsByTime(speed, speed, TIME)
+
+def blobDetectedCallback():
+    '''
+    Detecta un color, lo centra en su camara y se mueve hasta el sin llegar a chocar.
+    '''
+    global task_completed
+
+    color = BlobColor.RED
+    color_blob = robobo.readColorBlob(color)
+    
+    if color_blob.size <= 0:
+        return
+
+    if task_completed:
+        return
+
+    CENTER = 50
+    ERROR_MARGIN = 5
+
+    while abs(color_blob.posx - CENTER) > ERROR_MARGIN:
+        if color_blob.posx < CENTER - ERROR_MARGIN:
+            robobo.moveWheelsByTime(-15, 15, 0.3)  # Giro suave a la derecha
+        else:
+            robobo.moveWheelsByTime(15, -15, 0.3)  # Giro suave a la izquierda
+        
+        time.sleep(0.5)  
+        color_blob = robobo.readColorBlob(color)
+    # Si hay un color nos movemos a el
+    if color_blob.size > 0:
+        moveToAColor(color_blob.posx)
+        task_completed = True
+
+if __name__ == "__main__":
+    sim = RoboboSim(IP)
+    sim.connect()
+    sim.resetSimulation()
+
+    robobo = Robobo(IP)
+    robobo.connect()
+    robobo.moveTiltTo(110, 5)
+    robobo.setActiveBlobs(True, False, False, False)
+    robobo.whenANewColorBlobIsDetected(blobDetectedCallback)  # Corrección clave
+
+    try:
+        robobo.moveWheels(10, -10)  # Búsqueda giratoria
+        while True:
+            time.sleep(0.1)  # Reducir carga de CPU
+    except KeyboardInterrupt:
+        robobo.stopMotors()
+        sim.disconnect()
